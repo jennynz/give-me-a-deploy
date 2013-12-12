c3ca56ada3e696cbe20507ebe54b6fc9f7eb95c6
Jenny Sahng <jenny.sahng@orionhealth.com>
2013-12-12 17:04:12 +1300

    Freshened up page & code, centered all

--- js/script.js
+++ js/script.js
@@ -7,11 +7,11 @@ Copied.execCommand("Copy");
 }
 
 // Slider down to source code
-$(document).ready(function(){
+/*$(document).ready(function(){
   $("#up").click(function(){
     $("#slide").slideUp("slow","swing");
   });
   $("#down").click(function(){
     $("#slide").slideDown("slow","swing");
   });
-});
+});*/
