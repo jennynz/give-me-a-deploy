c4f8b13ede61851280684b0660295a63f57e6154
Jenny Sahng <jenny.sahng@orionhealth.com>
2013-12-17 13:57:20 +1300

    JS function to differentiate between code for three types of vmenvs

--- js/script.js
+++ js/script.js
@@ -68,13 +68,13 @@ function GenerateSourceCode(vmname,vmenv)
 		"# vi: set ft=ruby :" + '\n\n' +
 
 		"# Vagrantfile API/syntax version. Don't touch unless you know what you're doing!" + '\n' +
-		"VAGRANTFILE_API_VERSION = \"2\"" + '\n\n' +
+		"VAGRANTFILE_API_VERSION = '2'" + '\n\n' +
 
 		"Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|" + '\n\n' + 
 
 		"# Allocate the VM more memory" + '\n' + 
 		"config.vm.provider :virtualbox do |vb|" + '\n' + 
-		"  vb.customize [\"modifyvm\", :id, \"--memory\", \"1536\"]" + '\n' +
+		"  vb.customize ['modifyvm', :id, '--memory', '1536']" + '\n' +
 		"end" + '\n\n' + 
 		 
 		"# Every Vagrant VM requires a box to build off of." + '\n' +
@@ -84,23 +84,84 @@ function GenerateSourceCode(vmname,vmenv)
 		"config.vm.box_url = " + boxurl + '\n\n' +
 
 		"# Set the name of the host machine." + '\n' +
-	  	"config.vm.hostname = \"" + vmname + "\"" + '\n\n' + 
+	  	"config.vm.hostname = '" + vmname + "'" + '\n\n' + 
 
 		"# Create a forwarded port mapping which allows access to a specific port within the machine from a port on the host machine." + '\n' + 
 		"config.vm.network :forwarded_port, guest: 19080, host: 19080" + '\n\n' + 
 
 		"# Install Puppet" + '\n' + 
-	 	"config.vm.provision :shell, path: \"install_puppet.sh\"" + '\n\n' + 
+	 	"config.vm.provision :shell, path: 'install_puppet.sh'" + '\n\n' + 
 
 	 	"# Install OHP with Puppet modules" + '\n' + 
 		"config.vm.provision :puppet do |puppet|" + '\n' + 
-		"  puppet.module_path  = \"modules/puppet-ohp\"" + '\n' +
-		"  puppet.manifests_path = \"manifests\"" + '\n' +
-		"  puppet.manifest_file  = \"site.pp\"" + '\n' + 
+		"  puppet.module_path  = 'modules/puppet-ohp'" + '\n' +
+		"  puppet.manifests_path = 'manifests'" + '\n' +
+		"  puppet.manifest_file  = 'site.pp'" + '\n' + 
+		"end" + '\n\n' +
+
+		"end");
+
+	} else if (vmenv=="devstackenv") {
+		return ("DevStack yay");
+
+
+	} else if (vmenv=="hpcloudenv") {
+		return ("# -*- mode: ruby -*-" + '\n' +
+		"# vi: set ft=ruby :" + '\n\n' +
+
+		"# Vagrantfile API/syntax version. Don't touch unless you know what you're doing!" + '\n' +
+		"VAGRANTFILE_API_VERSION = '2'" + '\n\n' +
+
+		"Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|" + '\n\n' + 
+
+		"# Allocate the VM more memory" + '\n' + 
+		"config.vm.provider :virtualbox do |vb|" + '\n' + 
+		"  vb.customize ['modifyvm', :id, '--memory', '1536']" + '\n' +
+		"end" + '\n\n' + 
+		 
+		"# Every Vagrant VM requires a box to build off of." + '\n' +
+		"config.vm.box = 'dummy'" + '\n\n' + 
+
+		"# The url from where the config.vm.box will be fetched if it doesn't already exist on the host machine." + '\n' + 
+		"config.vm.box_url = 'https://github.com/cloudbau/vagrant-openstack-plugin/raw/master/dummy.box'" + '\n\n' +
+
+		"# Set the name of the host machine." + '\n' +
+	  	"config.vm.hostname = '" + vmname + "'" + '\n\n' + 
+
+		"config.vm.provider :openstack do |os|" + '\n' +
+		    "os.username = ENV['OS_USERNAME']" + '\n' +
+		    "os.api_key  = ENV['OS_PASSWORD']" + '\n' +
+		    "os.tenant   = ENV['OS_TENANT_NAME']" + '\n' +
+		    "os.endpoint = \"#{ENV['OS_AUTH_URL']}/tokens\"" + '\n\n' +
+
+		    "os.keypair_name = keypair_name" + '\n' +
+		    "os.ssh_username = 'root'" + '\n\n' +
+
+		    "os.flavor   = 'standard.small'" + '\n' +
+		    "os.image    = '78265' # CentOS 6.3" + '\n\n' +
+
+		    "os.address_id = 'private' # Chooses the 15.x.x.x range 'private IP' over the 10.x.x.x address" + '\n\n' +
+
+			"hostname = `hostname`.chomp" + '\n' +
+		    "os.server_name = '#{hostname}-vagrant'" + '\n\n' +
+
+		    "# Workaround until Vagrant 1.4 is released - https://github.com/mitchellh/vagrant/issues/1482" + '\n' +
+		    "os.user_data = File.read('user-data.txt')" + '\n' +
+		"end" + '\n\n' +
+
+		"# Install Puppet" + '\n' + 
+	 	"config.vm.provision :shell, path: 'install_puppet.sh'" + '\n\n' + 
+
+	 	"# Install OHP with Puppet modules" + '\n' + 
+		"config.vm.provision :puppet do |puppet|" + '\n' + 
+		"  puppet.module_path  = 'modules/puppet-ohp'" + '\n' +
+		"  puppet.manifests_path = 'manifests'" + '\n' +
+		"  puppet.manifest_file  = 'site.pp'" + '\n' + 
 		"end" + '\n\n' +
 
 		"end");
 	}
+
 }
 
 
