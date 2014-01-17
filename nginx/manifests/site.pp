93b5e2fbd689fba371a229a6474fbd9591be5edf
Jenny Sahng <jenny.sahng@orionhealth.com>
2014-01-17 15:16:44 +1300

    site.pp: updated, now hosts on 127.0.0.1:19080

--- nginx/manifests/site.pp
+++ nginx/manifests/site.pp
@@ -5,10 +5,20 @@ class { 'nginx': }
 nginx::resource::vhost { 'give-me-a-deploy':
   ensure               => present,
   server_name          => ['give-me-a-deploy'],
-  listen_port          => 80,
+  listen_port          => 19080,
   ssl                  => false,
-  www_root             => '/var/give-me-a-deploy',
+  www_root             => '/vagrant/html',
   use_default_location => false,
   access_log           => '/var/log/nginx/rpm-repo_access.log',
   error_log            => '/var/log/nginx/rpm-repo_error.log',
 }
+
+# sudo service nginx start
+# sudo service nginx stop
+
+# /usr/share/nginx/html/index.html
+
+# Might need an EPEL repo for RHEL CentOS?
+# http://wiki.nginx.org/NginxPlatformFedora
+
+# sudo puppet apply /vagrant/manifests/site.pp --modulepath=/vagrant/modules/:/etc/puppet/modules/
