5001e3daa1de8c332c1bb86b086faa708fd99644
Jenny Sahng <jenny.sahng@orionhealth.com>
2014-01-17 14:01:10 +1300

    Got rid of node default {}

--- nginx/manifests/site.pp
+++ nginx/manifests/site.pp
@@ -1,16 +1,14 @@
-node default {
-  # Install and bootstrap an NGINX instance
-  class { 'nginx': }
+# Install and bootstrap an NGINX instance
+class { 'nginx': }
 
-  # Setup a new virtual host
-  nginx::resource::vhost { 'give-me-a-deploy':
-    ensure               => present,
-    server_name          => ['give-me-a-deploy'],
-    listen_port          => 80,
-    ssl                  => false,
-    www_root             => '/var/give-me-a-deploy',
-    use_default_location => false,
-    access_log           => '/var/log/nginx/rpm-repo_access.log',
-    error_log            => '/var/log/nginx/rpm-repo_error.log',
-  }  
+# Setup a new virtual host
+nginx::resource::vhost { 'give-me-a-deploy':
+  ensure               => present,
+  server_name          => ['give-me-a-deploy'],
+  listen_port          => 80,
+  ssl                  => false,
+  www_root             => '/var/give-me-a-deploy',
+  use_default_location => false,
+  access_log           => '/var/log/nginx/rpm-repo_access.log',
+  error_log            => '/var/log/nginx/rpm-repo_error.log',
 }
